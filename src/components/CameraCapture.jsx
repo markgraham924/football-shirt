39a1f86c884d1ed67daffe4888c9d50ff29c3f21
Mark Graham <markgraham924@gmail.com>
2025-03-17 21:51:10 +0000

    Update CameraCapture.jsx

--- src/components/CameraCapture.jsx
+++ src/components/CameraCapture.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, stream } from 'react';
 import styles from '../styles/CameraCapture.module.css';
 import ShirtOverlay from './ShirtOverlay';
 
